3ab3180e2a05bc3cb6a09caea99632d600d68975
Rossiitaa <rosario.scuotto@outlook.it>
2022-07-15 10:12:34 +0200

    bonus

---
+++ src/components/jumbotron.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img src="../assets/JumbotronBasket.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    img {
+        width: 50%;
+        height: 50%;
+        object-fit: cover;
+    }
+</style>
