a873552efb79fc8bd2f7fd9900aa5c6daa43a9d9
Rossiitaa <rosario.scuotto@outlook.it>
2022-07-15 10:03:18 +0200

    MILESTONE

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste error, iure sunt aperiam ratione dolor iusto possimus tempore ut aliquid hic, deleniti eaque corrupti consequuntur commodi ex necessitatibus assumenda eveniet?</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Title'
+}
+</script>
+
+<style>
+
+</style>
